4ae870c744d59a549146a4c050ee0840605110b2
MarkusRabus <markus.rabus@gmail.com>
2018-08-04 19:31:39 -0400

    start watchdog

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@
 #cd MPG2.2m_pipeline/ceres && \
 #python install.py
 
+nohub python watcher.py &
+
 cd MPG2.2m_pipeline/django
 python manage.py migrate                  # Apply database migrations
 python manage.py collectstatic --noinput  # Collect static files
