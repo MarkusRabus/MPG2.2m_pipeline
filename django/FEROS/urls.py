47574c911eb146aff73dd70a0ac45b6f32482264
MarkusRabus <markus.rabus@gmail.com>
2018-08-04 20:33:40 -0400

    changed re_path to url

--- django/FEROS/urls.py
+++ django/FEROS/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import re_path
+from django.conf.urls import url
 
 from . import views
 
 app_name = "FEROS"
 
 urlpatterns = [
-    re_path(r'^$', views.index, name='index'),
-    re_path(r'^raw/$', views.raw, name='raw'),
-    re_path(r'^reduced/$', views.reduced, name='reduced'),
-    re_path(r'^longtermcal/$', views.longtermcal, name='longtermcal'),
-    re_path(r'^longtermrv/$', views.longtermrv, name='longtermrv'),
-    re_path(r'^help/$', views.help, name='help'),
-    re_path(r'^contact/$', views.contact, name='contact'),
-    re_path(r'^wavesol/$', views.wavesol, name='wavesol'),
-    re_path(r'^plotcal/$', views.plotcal, name='plotcal'),
-    re_path(r'^spectra/$', views.spectra, name='spectra'),
+    url(r'^$', views.index, name='index'),
+    url(r'^raw/$', views.raw, name='raw'),
+    url(r'^reduced/$', views.reduced, name='reduced'),
+    url(r'^longtermcal/$', views.longtermcal, name='longtermcal'),
+    url(r'^longtermrv/$', views.longtermrv, name='longtermrv'),
+    url(r'^help/$', views.help, name='help'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^wavesol/$', views.wavesol, name='wavesol'),
+    url(r'^plotcal/$', views.plotcal, name='plotcal'),
+    url(r'^spectra/$', views.spectra, name='spectra'),
 ]
